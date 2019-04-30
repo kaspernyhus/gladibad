1fb7bf6d84579d91202a4a9196d2131dceb7d22b
Kasper Nyhus <kanyhus@gmail.com>
2019-04-30 03:22:10 +0200

    4th

---
+++ main/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('changestate/<state>/', views.update_db, name='update_db'),
+    path('about', views.about, name='about'),
+]
+
