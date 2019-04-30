2ee655a9b1d23ae4c4b7e633cdb3e773f2d30cf4
Kasper Nyhus <kanyhus@gmail.com>
2019-04-30 11:38:59 +0200

    beta v1.0

--- main/views.py
+++ main/views.py
@@ -3,40 +3,49 @@ from .models import BrState
 from datetime import datetime
 
 def index(request):
-    #get data(object) from db    
-    db_data = BrState.objects.latest('id')
+    try:
+        #get data(object) from db    
+        db_data = BrState.objects.latest('id')
+        
+        # time since db timestamp
+        db_timestamp = db_data.timestamp
+        db_timestamp_str = str(db_timestamp)
+
+        # make proper datetime object from db_data
+        timestamp_object = datetime.strptime(db_timestamp_str, '%Y-%m-%d %H:%M:%S.%f+00:00')
+
+        now = datetime.now()
+
+        time_passed = now - timestamp_object
+        time_passed = time_passed.total_seconds()
+        time_passed_in_m = int(time_passed / 60)
     
-    # time since db timestamp
-    db_timestamp = db_data.timestamp
-    db_timestamp_str = str(db_timestamp)
-
-    # make proper datetime object from db_data
-    timestamp_object = datetime.strptime(db_timestamp_str, '%Y-%m-%d %H:%M:%S.%f+00:00')
-
-    now = datetime.now()
-
-    time_passed = now - timestamp_object
-    time_passed = time_passed.total_seconds()
-    time_passed_in_m = int(time_passed / 60)
-
-
-    if db_data.state == 0:
-        context = {'state': db_data.state, 'status': 'LEDIGT', 'time_passed': time_passed_in_m}
-    elif db_data.state ==1:
-        context = {'state': db_data.state, 'status': 'OPTAGET', 'time_passed': time_passed_in_m, 'in_que': db_data.que}
-
+        if db_data.state == 0:
+            context = {'state': db_data.state, 'status': 'LEDIGT', 'time_passed': time_passed_in_m}
+        elif db_data.state ==1:
+            context = {'state': db_data.state, 'status': 'OPTAGET', 'time_passed': time_passed_in_m, 'in_que': db_data.que}
     
+    except:
+        context = {'state': 0, 'status': 'No Data', 'time_passed': 0}
+
+
     return render(request, 'index.html', context)
 
 
 def update_db(request, state):
-    old_state = BrState.objects.latest('id')
-    in_que = old_state.que
-
-    timestamp = datetime.now()
-    new_state = BrState(state=state, timestamp=timestamp, que=in_que)
-    new_state.save()
     
+    try:
+        old_state = BrState.objects.latest('id')
+        in_que = old_state.que
+
+        timestamp = datetime.now()
+        new_state = BrState(state=state, timestamp=timestamp, que=in_que)
+        new_state.save()
+    except:
+        timestamp = datetime.now()
+        new_state = BrState(state=state, timestamp=timestamp, que='0')
+        new_state.save()
+
     context = {'state': state, 'timestamp': timestamp}
     return render(request, 'updated.html', context)
 
