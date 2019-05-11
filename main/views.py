3bea676054e9dbbeb8c74bca54901e7e8b386b83
Kasper Nyhus <kanyhus@gmail.com>
2019-05-11 22:05:40 +0200

    updated stats views

--- main/views.py
+++ main/views.py
@@ -57,15 +57,42 @@ def about(request):
 def stats(request):
     db_data = BrState.objects.all()
 
-    timestamps = []
+    on_timestamps = []
+    off_timestamps = []
 
     for data in db_data:
-        state_time = {'state': data.state, 'timestamp': data.timestamp}
-        timestamps.append(state_time)
+        if data.state == 1:
+            on_timestamps.append(data.timestamp)
+        elif data.state == 0:
+            off_timestamps.append(data.timestamp)
+ 
+    durations = []
+    
+    if on_timestamps[0] < off_timestamps[0]:
+        for i, on_time in enumerate(on_timestamps):
+            on_timestamp_object = datetime.strptime(str(on_time), '%Y-%m-%d %H:%M:%S.%f')
+            off_timestamp_object = datetime.strptime(str(off_timestamps[i]), '%Y-%m-%d %H:%M:%S.%f')
 
-    timestamps = timestamps[-20:]
+            duration = off_timestamp_object.replace(microsecond=0) - on_timestamp_object.replace(microsecond=0)
 
-    context = {'timestamps': timestamps}
+            durations.append(duration)
+
+    else:
+        off_timestamps = off_timestamps[1:]
+        for i, on_time in enumerate(on_timestamps[:-1]):
+            on_timestamp_object = datetime.strptime(str(on_time), '%Y-%m-%d %H:%M:%S.%f')
+            off_timestamp_object = datetime.strptime(str(off_timestamps[i]), '%Y-%m-%d %H:%M:%S.%f')
+
+            duration = off_timestamp_object.replace(microsecond=0) - on_timestamp_object.replace(microsecond=0)
+
+            durations.append(duration)
+
+
+
+
+    packed_times = list(zip(on_timestamps, off_timestamps, durations))
+
+    context = {'times_durations': packed_times}
 
     return render(request, 'stats.html', context)
 
