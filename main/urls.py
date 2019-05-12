9929faf6fb158d0ad27220120ccded20e14012e0
Kasper Nyhus <kanyhus@gmail.com>
2019-05-12 23:41:41 +0200

    vip features added

--- main/urls.py
+++ main/urls.py
@@ -4,9 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('vip/', views.vip_index, name='vip_index'),
     path('changestate/<state>/', views.update_db, name='update_db'),
     path('stats/', views.stats, name='stats'),
     path('stats<int:entries_requested>/', views.stats, name='stats'),
     path('about', views.about, name='about'),
+    path('notify_me<int:slack_user>/', views.notify_me, name="notify_me"),
 ]
 
--- main/views.py
+++ main/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import BrState
 from datetime import datetime, timedelta
+from django.http import HttpResponseRedirect    
+import slack
 
 def index(request):
     try:
@@ -23,7 +25,7 @@ def index(request):
         if db_data.state == 0:
             context = {'state': db_data.state, 'status': 'LEDIGT', 'time_passed': time_passed_in_m, 'current_time': now}
         elif db_data.state == 1:
-            context = {'state': db_data.state, 'status': 'OPTAGET', 'time_passed': time_passed_in_m, 'current_time': now, 'in_que': db_data.que}
+            context = {'state': db_data.state, 'status': 'OPTAGET', 'time_passed': time_passed_in_m, 'current_time': now}
         
     except:
         context = {'state': 0, 'status': 'No Data', 'time_passed': 0}
@@ -38,10 +40,13 @@ def update_db(request, state):
         status = 'Error: Repeated state'
 
     else:
-        new_state = BrState(state=state, timestamp=timestamp, que='0')
+        if (old_state.notifier > 0 and state == 'False'):
+            send_notification(old_state.notifier)
+        
+        new_state = BrState(state=state, timestamp=timestamp, notifier=0)
         new_state.save()
         status = 'DB updated succesfully'
-    
+
     context = {'status': status, 'state': state, 'timestamp': timestamp}
     return render(request, 'updated.html', context)
 
@@ -90,13 +95,62 @@ def stats(request, entries_requested=10):
     return render(request, 'stats.html', context)
 
 
-def update_que():
-    old_state = BrState.objects.latest('id')
-    in_que = old_state.que
+def notify_me(request, slack_user):
+    db_data = BrState.objects.latest('id')
 
-    timestamp = old_state.timestamp
-    state = old_state.state
+    db_data.notifier = slack_user
+    db_data.save(update_fields=['notifier'])
 
-    new_state = BrState(state=state, timestamp=timestamp, que=in_que)
-    new_state.save()
-    reload()
+    return HttpResponseRedirect('/')
+
+
+def vip_index(request):
+    try:
+        #get data(object) from db    
+        db_data = BrState.objects.latest('id')
+        
+        # time since db timestamp
+        db_timestamp = db_data.timestamp
+        db_timestamp_str = str(db_timestamp)
+
+        # make proper datetime object from db_data
+        timestamp_object = datetime.strptime(db_timestamp_str, '%Y-%m-%d %H:%M:%S.%f')
+        
+        now = datetime.now()
+
+        time_passed = now - timestamp_object
+        time_passed = time_passed.total_seconds()
+        time_passed_in_m = int(time_passed / 60)
+        
+        if db_data.state == 0:
+            context = {'state': db_data.state, 'status': 'LEDIGT', 'time_passed': time_passed_in_m, 'current_time': now}
+        elif db_data.state == 1:
+            context = {'state': db_data.state, 'status': 'OPTAGET', 'time_passed': time_passed_in_m, 'current_time': now}
+        
+    except:
+        context = {'state': 0, 'status': 'No Data', 'time_passed': 0}
+    
+    return render(request, 'vip_index.html', context)
+
+
+def send_notification(notifier):
+    print('-------- Notification send!!! -------')
+    slack_token = "xoxp-621793017634-633183116789-635389614102-d7b1d2b9aff8950a6e1a4b1109afc2d2"
+    client = slack.WebClient(token=slack_token)
+
+    if notifier == 1:
+        response = client.chat_postMessage(
+            channel='#gladibad_laura',
+            text="Baddet er ledigt nu!")
+        assert response["ok"]
+        assert response["message"]["text"] == "Baddet er ledigt nu!"
+    elif notifier == 2:
+        response = client.chat_postMessage(
+            channel='#gladibad_kasper',
+            text="Baddet er ledigt nu!")
+        assert response["ok"]
+        assert response["message"]["text"] == "Baddet er ledigt nu!"
+
+    
+
+        
