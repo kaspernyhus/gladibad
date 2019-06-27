dc2ccb1660c2ba4a48ebe9ef2bab58aa8fa086c5
Kasper Nyhus <kanyhus@gmail.com>
2019-06-28 00:35:37 +0200

    more_stats rettelse

--- stats/views.py
+++ stats/views.py
@@ -11,17 +11,10 @@ def stats(request, entries_requested=10):
 
 def more_stats(request, entries_requested='alle'):
     stats = get_stats(entries_requested)
-
-    weekdays = [['Mandag'], ['Tirsdag'], ['Onsdag'], ['Torsdag'], ['Fredag'], ['Lørdag'], ['Søndag']]
     
     avgs = get_time_avereges(stats)
     most_busy = get_most_busy(stats)
     longest_shower = get_longest_shower(stats)
 
     context = {'requested_entries': entries_requested, 'avgs': avgs, 'most_busy': most_busy, 'longest_shower': longest_shower }
-    
-    
     return render(request, 'more_stats.html', context)
-
-
-        
