85519534861319c25389d0b452fe17668bf10134
Kasper Nyhus <kanyhus@gmail.com>
2019-10-04 10:27:07 +0200

    models fix

--- main/models.py
+++ main/models.py
@@ -3,3 +3,5 @@ from django.db import models
 class BrState(models.Model):
     state = models.BooleanField(default=False)
     timestamp = models.DateTimeField(auto_now=True)
+    notifier = models.CharField(max_length=1000, default=[{}])
+    
--- main/views.py
+++ main/views.py
@@ -39,11 +39,7 @@ def update_db(request, state):
         status = 'Error: Repeated state'
 
     else:
-<<<<<<< HEAD
         new_state = BrState(state=state, timestamp=timestamp, notifier=notifier)
-=======
-        new_state = BrState(state=state, timestamp=timestamp, notifier='0')
->>>>>>> 4d7551e1fdc51ffe55d60c2ff82d3657d6da0c69
         new_state.save()
         status = 'DB updated succesfully'
 
@@ -55,39 +51,5 @@ def about(request):
     return render(request, 'about.html')
 
     
-<<<<<<< HEAD
 
         
-=======
-    for data in db_data:
-        if data.state == 1:
-            on_timestamps.append(data.timestamp)
-        elif data.state == 0:
-            off_timestamps.append(data.timestamp)
-    
-    durations = []
-    
-    if on_timestamps[0] < off_timestamps[0]:
-        for i, on_time in enumerate(on_timestamps):
-            on_timestamp_object = datetime.strptime(str(on_time), '%Y-%m-%d %H:%M:%S.%f')
-            off_timestamp_object = datetime.strptime(str(off_timestamps[i]), '%Y-%m-%d %H:%M:%S.%f')
-
-            duration = off_timestamp_object.replace(microsecond=0) - on_timestamp_object.replace(microsecond=0)
-
-            durations.append(duration)
-
-    else:
-        on_timestamps = on_timestamps[1:]
-        for i, on_time in enumerate(on_timestamps):
-            on_timestamp_object = datetime.strptime(str(on_time), '%Y-%m-%d %H:%M:%S.%f')
-            off_timestamp_object = datetime.strptime(str(off_timestamps[i]), '%Y-%m-%d %H:%M:%S.%f')
-
-            duration = off_timestamp_object.replace(microsecond=0) - on_timestamp_object.replace(microsecond=0)
-
-            durations.append(duration)
-
-    packed_times = list(zip(on_timestamps, off_timestamps, durations))
-
-    context = {'requested_entries': entries_requested, 'times_durations': packed_times}
-    return render(request, 'stats.html', context)
->>>>>>> 4d7551e1fdc51ffe55d60c2ff82d3657d6da0c69
